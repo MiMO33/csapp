4c21aceb11b7c98aafab7b2e8775c33f92fc67df
MiMO33 <MiMO33@users.noreply.github.com>
2021-08-03 19:44:26 +0800

    cache lab part A

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,173 +1,156 @@
-#include <unistd.h>     /* getopt*/
-#include <getopt.h>     /* getopt*/
-#include <stdio.h>      /* fopen fscanf*/
-#include <stdlib.h>     /* atoi callloc*/
-#include <string.h>     /* strcpy*/
-#include "cachelab.h"   /* printSummary*/
+#include "cachelab.h"
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
 
-int time = 0;
-
-typedef struct {
-    long CI;
-    long CO;
-    long CT;
-} address;
+int visit_cache_line(unsigned long addr);
 
 typedef struct {
+    int valid;
     int tag;
-    long address;
-    long time_stamp;
+    int stamp;
 }cache_line;
 
-typedef struct {
-    int s, E, b, t;
-} cashe_arg;
 
-typedef struct {
-    long CI;
-    long CT;
-    long CO;
-}addr_con;
+int h,v,s,E,b,B,S,t;
+int hits , misses , evictions;
+cache_line** cache;
+FILE* fp = NULL;
 
-long get_mask(int t) {
-    long mask = 0;
-    for(int i = 0; i < t; i++) {
-        mask = (mask << 1) + 1;
-    }
-    return mask;
+void  updateStamp(int s,int E)
+{
+    for(int i = 0; i < 1<<s; ++i)
+        for(int j = 0; j < E; ++j)
+            if(cache[i][j].valid == 1)
+                ++cache[i][j].stamp;
 }
 
-addr_con* split_addr(long addr_in, int s, int t, int b) {
-    addr_con* ret = malloc(sizeof(addr_con));
-    ret->CO = get_mask(b) & addr_in;
-    ret->CI = ((get_mask(s) << b) & addr_in) >> b;
-    ret->CT = ((get_mask(t) << (s + b)) & addr_in) >> (s + b);
-    return ret;
+void print_usage()
+{
+    printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+            "Options:\n"
+            "  -h         Print this help message.\n"
+            "  -v         Optional verbose flag.\n"
+            "  -s <num>   Number of set index bits.\n"
+            "  -E <num>   Number of lines per set.\n"
+            "  -b <num>   Number of block offset bits.\n"
+            "  -t <file>  Trace file.\n\n"
+            "Examples:\n"
+            "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+            "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
 }
 
-int hit(cache_line* cache, addr_con* addr, int E, int t, int s) {
-
-    for(int i = 0; i < E; i++) {
-        if((cache[addr->CI * E + i]).tag == 1     &&
-            cache[addr->CI * E + i].address == addr->CT) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int find_min(cache_line* cache, addr_con* addr, int E) {
-
-    int ret = 0;
-    for(int i = 0; i < E; i++) {
-        if(cache[addr->CI + ret].time_stamp < cache[addr->CI + i].time_stamp) {
-            ret = i;
-        }
-    }
-    return ret;
-}
-
-int main(int argc, char** argv) {
-
-    
-    int opt, hits = 0, misses = 0, evictions = 0;
-    int s, E, b, t, flag_v = 0;
-    char* file_path = (char*)malloc(sizeof(char) * 128);
-
+int main(int argc, char* argv[])
+{
+    int opt;
+    // int verbose_flag = 0;
     while(-1 != (opt = getopt(argc, argv, "hvs:E:b:t:"))) {
         switch (opt) {
-        case 'h':
-            printf("Usage:                                              \
-                    ./csim-ref [-hv] -s <s> -E <E> -b <b> -t <tracefile>");
-            break;
-        case 'v':
-            flag_v = 1;
-            break;
         case 's':
             s = atoi(optarg);
             break;
+        case 'h':
+            print_usage(); break;
+        case 'v':
+             break;
         case 'E':
-            E = atoi(optarg);
-            break;
+            E = atoi(optarg); break;
         case 'b':
-            b = atoi(optarg);
-            break;
+            b = atoi(optarg); break;
         case 't':
-            strcpy(file_path, optarg);
-            break;
-        default:
-            printf("wrong argument\n");
-            break;
+			fp = fopen(optarg, "r"); break;
         }
-    }
+    } 
     t = 64 - s - b;
+    S = pow(2, s);
+    B = pow(2, s);
 
-    cache_line* cache = (cache_line*) calloc(s * E, sizeof(cache_line));
-    char access_type;
-    long addr_in;
-    int size;
-    FILE *fp = fopen(file_path, "r");
-    while(fscanf(fp, "%c %lx,%d", &access_type, &addr_in, &size) > 0) {
-
-        addr_con* addr = split_addr(addr_in, s, t, b);
-        switch (access_type) {
-        case 'I':
-            break;
-        case 'M':
-            if(flag_v) { printf("%c %lx,%d ", access_type, addr_in, size);}
-            if(hit(cache, addr, E, s, t) == -1) {
-                if(flag_v) printf("miss");
-                misses++;
-                int min = find_min(cache, addr, E);
-                cache[addr->CI + min].tag = 1;
-                cache[addr->CI + min].address = addr->CT;
-                if(cache[addr->CI + min].time_stamp != 0) {
-                    printf(" evictions");
-                    evictions++;
-                }
-                cache[addr->CI + min].time_stamp = (++time);
-                if(flag_v) printf(" hit");
-                hits++;
-                //printf("--->%ld\n", addr->CI + min);
-                printf("\n");
-                break;
-            } else {
-                if(flag_v) printf("hit hit\n");
-                hits += 2;
-                int i = hit(cache, addr, E, s, t);
-                cache[addr->CI + i].time_stamp = (++time);
-                printf("\n");
-                break;
-            }
-            break;
-        case 'S':
-        case 'L':
-            if(flag_v) {printf("%c %lx,%d ", access_type, addr_in, size);}
-            if(hit(cache, addr, E, s, t) == -1) {
-                if(flag_v) printf("miss");
-                misses++;
-                int min = find_min(cache, addr, E);
-                cache[addr->CI + min].tag = 1;
-                cache[addr->CI + min].address = addr->CT;
-                if(cache[addr->CI + min].time_stamp != 0) {
-                    printf(" evictions");
-                    evictions++;
-                }
-                cache[addr->CI + min].time_stamp = (++time);
-                //printf("--->%ld\n", addr->CI + min);
-                break;
-            } else {
-                if(flag_v) printf("hit");
-                hits++;
-                int i = hit(cache, addr, E, s, t);
-                cache[addr->CI + i].time_stamp = (++time);
-                printf("\n");
-                break;
-            }
+    //init cache
+    cache = ( cache_line**)malloc(sizeof(cache_line*) * S);
+    for(int i=0;i<S;i++){
+        cache[i]=( cache_line*)malloc(sizeof( cache_line)*E);
+        for(int j=0;j<E;j++){
+            cache[i][j].tag=-1;
+            cache[i][j].stamp=-1;
+            cache[i][j].valid=0;
         }
     }
+
+    char buf[20], operation;
+    unsigned long address;
+    int size;
+
+    while(fgets(buf, sizeof(buf), fp) != NULL) {
+        if(buf[0] == 'I')
+            continue;
+        sscanf(buf, " %c %lx,%d", &operation, &address, &size);
+
+        switch(operation) {
+            case 'L': 
+            case 'S':
+                visit_cache_line(address);
+                break;
+            case 'M':
+                visit_cache_line(address);
+                visit_cache_line(address);
+                break;
+        }
+        updateStamp(s, E);
+    }
+
+    printSummary(hits, misses, evictions);
+
+    for (int i = 0; i < S; i++)
+        free(cache[i]);
     free(cache);
     fclose(fp);
-    printSummary(hits , misses, evictions);
     return 0;
+}
+
+/**
+ * return value
+ * 0: hit
+ * 1: miss
+ * 2: cache miss, eviction
+ */
+int visit_cache_line(unsigned long addr) 
+{
+    int tag = addr >> (s + b);
+    int index = (addr >> b) & ((-1U) >> (64 - s));
+    int MaxStamp=INT_MIN;
+    int MaxStamp_index=-1;
+    //printf("%lx\n", ((~0UL)  >> (64 - s)));
+    for(int i = 0; i < E; i++) {
+        if(cache[index][i].tag == tag) {
+            cache[index][i].stamp=0;
+            hits++;
+            return 0;
+        }
+    }
+
+    for(int i = 0; i < E; i++) {
+        if(cache[index][i].valid == 0) {
+            cache[index][i].valid = 1;
+            cache[index][i].tag = tag;
+            cache[index][i].stamp=0;
+            misses++;
+            return 1;
+        }
+    }
+
+    misses++;
+    evictions++;
+    for(int i = 0; i < E; ++i) {
+        if(cache[index][i].stamp > MaxStamp) {
+            MaxStamp = cache[index][i].stamp;
+            MaxStamp_index = i;
+        }
+    }
+    cache[index][MaxStamp_index].tag = tag;
+    cache[index][MaxStamp_index].stamp = 0;
+    return 2;
 }
